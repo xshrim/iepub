3c7dc46aea0b64c73ab29d9ead796038e688f734
xshrim <xshrim@gmail.com>
2026-03-24 19:40:10 +0800

    add features

--- main.go
+++ main.go
@@ -1849,6 +1849,11 @@ func getAiTitle(llm, chapterContent string, wait int) (string, string) {
 		if len(models) == 0 {
 			models = []string{"Qwen/Qwen3-8B", "Qwen/Qwen2.5-7B-Instruct", "Qwen/Qwen2-7B-Instruct", "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B", "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B", "Qwen/Qwen3.5-4B", "THUDM/GLM-4.1V-9B-Thinking", "THUDM/glm-4-9b-chat", "THUDM/GLM-Z1-9B-0414", "THUDM/GLM-4-9B-0414", "tencent/Hunyuan-MT-7B"}
 		}
+	case "or", "openrouter":
+		llmurl = "https://openrouter.ai/api/v1/chat/completions"
+		if len(models) == 0 {
+			models = []string{"openrouter/free"}
+		}
 	default:
 		return "", ""
 	}
